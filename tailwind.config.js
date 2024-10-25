607ae1c50cba8e750d9fd2a9f1ef5ae62fdc4131
wata58 <bon.voyage.1357@gmail.com>
2024-10-25 10:28:07 +0900

    40.ユーザーの登録機能を作ろう1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-//ファイル名:tailwind.config.js
+/*ファイル名：tailwind.config.js*/
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
