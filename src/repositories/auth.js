0074f881a1a90311b26949214ced0e0df9915854
wata58 <bon.voyage.1357@gmail.com>
2024-10-26 09:23:53 +0900

    41.ユーザーの登録機能を作ろう2

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -4,7 +4,7 @@ import { supabase } from "../lib/supabase";
 
 export const authRepository = {
   async signup(name, email, password) {
-    const { data, error } = await supabase.auth.signup({
+    const { data, error } = await supabase.auth.signUp({
       email,
       password,
       options: { data: { name } },
