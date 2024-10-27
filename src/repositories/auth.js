c1a57b397a242a49476d5748af0dc13d64f95097
wata58 <bon.voyage.1357@gmail.com>
2024-10-27 12:04:05 +0900

    42.ユーザーのログイン機能を作ろう

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -12,4 +12,16 @@ export const authRepository = {
     if (error != null) throw new Error(error.message);
     return { ...data.user, userName: data.user.user_metadata.name };
   },
+
+  async signin(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error != null) throw new Error(error.message);
+    return {
+      ...data.user,
+      userName: data.user.user_metadata.name,
+    };
+  },
 };
